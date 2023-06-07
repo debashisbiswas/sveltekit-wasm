18911806df1cc2b19eed0af9a1958080143098f5
Debashis Biswas <debashis.biswas@asu.edu>
2023-06-06 23:22:44 -0700

    Add wasm-pack template and set up with vite

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script lang="ts">
+  import init, { greet } from "hello-wasm";
+  import { onMount } from "svelte";
+
+  onMount(async () => {
+    await init();
+    greet();
+  });
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
